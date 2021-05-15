1fe17a6866b416dd77a18cc264b3fbaf6bcc051b
Kiltro <felipe@galaz.de>
2021-05-15 05:49:51 -0400

    tercera revision

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,20 +5,49 @@
     justify="center"
   >
     <v-row
-      class="ma-0 mt-10 pa-0"
-      style="width:100%;height:14px"
+      class="ma-0 pa-0"
+      style="width:100%"
       align="center"
       justify="center"
     >
-      <p>Private Policy | Modern Slavery Statement | Accesibility</p>
+      <v-row
+        class="ma-0 pa-0"
+        style="height:60px"
+        align="center"
+        justify="center"
+      >
+        <v-icon
+          class="ma-0 mr-2 pa-0"
+
+          color= "black"
+          @click="site('https://www.linkedin.com/company/experiencialab')"
+        >
+          mdi-linkedin
+        </v-icon>
+        <v-icon
+          class="ma-0 mr-2 pa-0"
+          color= "black"
+          @click="site('https://twitter.com/experiencia_lab')"
+        >
+          mdi-twitter
+        </v-icon>
+        <v-icon
+          class="ma-0 mr-2 pa-0"
+
+          color= "black"
+          @click="site('https://www.instagram.com/experiencialab/?hl=en')"
+        >
+          mdi-instagram
+        </v-icon>
+      </v-row>
     </v-row>
     <v-row
       class="ma-0 pa-0"
-      style="width:100%;height:14px"
+      style="width:100%;height:4px"
       align="center"
       justify="center"
     >
-      <p>Connect with us</p>
+      <p class="ma-0 pa-0 text-decoration-none">Designed & Developed by <a class="ma-0 pa-0 text-decoration-none" style="color:black;font-weight:bold" href="https://pinkdogstudio.com">Pink Dog</a></p>
     </v-row>
     <v-row
       class="ma-0 mt-3 pa-0 pb-10"
@@ -38,9 +67,20 @@
   </v-row>
 </template>
 
-<script>
 
+<script>
+  export default {
+    data: () => ({
+    }),
+    components: {},
+    methods: {
+      site: function (link) {
+        window.open(link, "_blank");
+      }
+    }
+  }
 </script>
+
 
 <style lang="sass">
 
