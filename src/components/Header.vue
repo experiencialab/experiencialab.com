62eb3190a864bdc563c30301beb67420a10714d9
Kiltro <felipe@galaz.de>
2021-06-06 23:26:41 -0400

    entrega final

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -57,7 +57,7 @@
           </a>
         </v-row>
         <v-row
-          class="ma-0 mx-1 pa-0"
+          class="ma-0 mx-4 pa-0"
           style="color:black;letter-spacing:0px;max-width:100px;transform-origin:center"
           align="center"
           justify="center"
