e08ad5567a3847fb57ba130b0b3a67deafc71dc1
Kiltro <felipe@galaz.de>
2021-06-07 15:58:35 -0400

    entrega final

--- src/components/Experiencias_Desktop.vue
+++ src/components/Experiencias_Desktop.vue
@@ -87,7 +87,7 @@
         </v-row>
       </v-row>
       <v-row
-        class="ma-0 mt-8 mb-16 pa-0 pt-16"
+        class="ma-0 mt-16 mb-8 pa-0"
         style="width:100vw;transform:scale(0.85)"
         v-for="experiencia in $store.state.experiencias"  :key="experiencia.index"
       >
